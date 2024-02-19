36ac1c8861f47212edb8d33505262e2343da3425
Элеонора Попова <kulmk@yandex.ru>
2024-02-19 12:54:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osipovgit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 64;
+            int y = 8;
+            Console.WriteLine($"Вывод: {x/y}");
+        }
+    }
+}
